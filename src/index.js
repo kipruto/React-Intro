fe709245ed65d9555f5a1bdb9f6c69cce5d10ce1
kipruto <barxwells@gmail.com>
2019-09-26 19:31:32 +0300

    react multiple input form

--- src/index.js
+++ src/index.js
@@ -4,23 +4,24 @@ import ReactDOM from 'react-dom'
 class MyForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { username: '' };
+        this.state = { username: '', age: null };
     }
-    mySubmitHandler = (event) => {
-        event.preventDefault();
-        alert("You are submitting " + this.state.username);
-    }
+
     myChangeHandler = (event) => {
-        this.setState({ username: event.target.value });
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({ [nam]: val });
     }
     render() {
         return (
-            <form onSubmit={this.mySubmitHandler}>
-                <h1>Hello {this.state.username}</h1>
-                <p>Enter your name and submit:</p>
-                <input type="text" onChange={this.myChangeHandler} />
+            <form >
+                <h1>Hello {this.state.username} {this.state.age}</h1>
+                <p>Enter your name:</p>
+                <input type="text" name="username" onChange={this.myChangeHandler} />
 
-                <input type="submit" />
+                <p>Enter your age:</p>
+                <input type="text" name="age" onChange={this.myChangeHandler} />
+
             </form>
         )
     }
