7aeefc6d5f47ce8ddff5ba5744bf3c1e028a0586
kipruto <barxwells@gmail.com>
2019-09-26 16:00:36 +0300

    React function component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class Car extends React.Component {
-    render() {
-        return <h2>Hi, I am a car!</h2>
-    }
+function Car() {
+    return <h2>Hi, I am also a Car!</h2>
 }
 
-ReactDOM.render(<Car/>, document.getElementById("root"))
+ReactDOM.render(<Car />, document.getElementById("root"));
