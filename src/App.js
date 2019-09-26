77b3433e6592a22f1b2319f23354e045efe94603
kipruto <barxwells@gmail.com>
2019-09-26 16:31:57 +0300

    components in files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Car extends React.Component {
+    render() {
+        return <h2>Hello, I am a car!</h2>
+    }
+}
+
+export default Car;
