0a8efea52620bad4c91cbd498fd1b2bf3e4f58b3
Kipruto Barno <barxwells@gmail.com>
2019-09-26 15:46:43 +0300

    JSX html with one top element

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const myElement=(
-    <ul>
-        <li>Apples</li>
-        <li>Bananas</li>
-        <li>Cheries</li>
-    </ul>
+const myElement = (
+    <div>
+        <h1>I am a Header.</h1>
+        <h1>I am a Header too.</h1>
+    </div>
 )
 
 ReactDOM.render(myElement, document.getElementById('root'));
